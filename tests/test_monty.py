f3124a0e14a68eceaf973ce9c30c63c056ef9a07
Jon Fazzaro <jon@fazzaro.com>
2023-02-22 09:15:18 -0500

    clean: tests

--- tests/test_monty.py
+++ tests/test_monty.py
@@ -8,34 +8,31 @@ class TestMonty(unittest.TestCase):
     def test_zero_items_returns_zero(self):
         self.assertEqual(
             [(75, 0), (85, 0), (95, 0)],
-            self.forecast(0, [0, 0, 0]))  # add assertion here
+            self.__forecast(0, [0, 0, 0]))
 
     def test_zero_data_returns_zero(self):
         self.assertEqual(
             [(75, 0), (85, 0), (95, 0)],
-            self.forecast(10, [0, 0, 0]))  # add assertion here
+            self.__forecast(10, [0, 0, 0]))
 
     def test_one_hundred_items(self):
         self.assertEqual(
             [(75, 16), (85, 25), (95, 32)],
-            self.forecast(100, [10, 8, 9, 4, 1, 4, 4, 2, 10, 20, 6, 7, 8]))  # add assertion here
+            self.__forecast(100, [10, 8, 9, 4, 1, 4, 4, 2, 10, 20, 6, 7, 8]))
 
     def test_three_hundred_items(self):
         self.assertEqual(
-            [(75, 7), (85, 9), (95, 14)],
-            self.forecast(300, [0, 23, 56, 34, 54, 56, 76, 1]))  # add assertion here
+            [(75, 6), (85, 9), (95, 14)],
+            self.__forecast(300, [0, 23, 56, 34, 54, 56, 76, 1]))
 
     def setUp(self):
         self.mockChoiceCalls = 1
 
-    def mock_choice(self, data):
+    def __mock_choice(self, data):
         self.mockChoiceCalls += 1
         seed = self.mockChoiceCalls / max(1, sum(data))
         index = round(seed * 10) % len(data)
         return data[index]
 
-    def forecast(self, items, data):
-        return Monty(data,
-                     passes=100,
-                     choose=self.mock_choice)\
-            .forecast(items)
+    def __forecast(self, items, data):
+        return Monty(data, choose=self.__mock_choice).forecast(items)
