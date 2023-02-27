093f59073fad3088a9d1dfd058f45207da53f6dc
Jon Fazzaro <jon@fazzaro.com>
2023-02-27 12:06:27 -0500

    feat: optional passes arg

--- main.py
+++ main.py
@@ -4,9 +4,9 @@ from monty import Monty
 
 
 def monty(items, samples, probabilities=None, passes=None):
-    simulation = Monty(samples=samples, items=items)
+    simulation = Monty(samples=samples, items=items, passes=passes or 10000)
     return list(map(
-        lambda p: f"{simulation.forecast(p)} iterations (a {p}% chance)",
+        lambda p: f"{round(simulation.forecast(p))} iterations (a {p}% chance)",
         probabilities or [75, 85, 95]))
 
 
